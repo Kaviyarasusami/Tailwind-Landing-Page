83af17db03be10698a94a71f92f21c4a4a804086
Kaviyarasusami <kaviyarasusami@gmail.com>
2024-09-14 17:49:43 +0530

    first comit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        cfont1: ['Righteous', 'sans-serif'],
+        cfont2: ['Saira', 'sans-serif'],
+      },
+
+      colors:{
+        linear:"#083F9B"        
+    },
+      textColor:{
+        bluecolor:"#0E38CD"
+      },
+    backgroundColor:{
+      purblecolor:""
+        
+    }
+
+      
+    },
+    
+  plugins: [],
+}
+}
